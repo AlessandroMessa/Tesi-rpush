3d2bb7e58e1bd8f3ac38eb681824cc8b60b375aa
Diagon <a.messa7@campus.unimib.it>
2025-08-06 10:11:38 +0200

    Update MessageHandlerHolder.java
    Stessa cosa, inietto MessageHandlerHolder

--- rpush/rpush-route/src/main/java/com/regent/rpush/route/utils/MessageHandlerHolder.java
+++ rpush/rpush-route/src/main/java/com/regent/rpush/route/utils/MessageHandlerHolder.java
@@ -1,7 +1,7 @@
 package com.regent.rpush.route.utils;
 
 import com.regent.rpush.dto.enumration.MessageType;
-import com.regent.rpush.route.handler.MessageHandler;
+import com.regent.rpush.route.spi.IMessageHandlerDescriptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.ApplicationContext;
@@ -24,21 +24,20 @@ public class MessageHandlerHolder implements CommandLineRunner {
     /**
      * 系统所有的消息处理器
      */
-    private static final Map<MessageType, MessageHandler<?>> HANDLER_MAP = new LinkedHashMap<>();
+    private static final Map<MessageType, IMessageHandlerDescriptor> HANDLER_MAP = new LinkedHashMap<>();
 
-    public static MessageHandler<?> get(MessageType messageType) {
+    public static IMessageHandlerDescriptor get(MessageType messageType) {
         return HANDLER_MAP.get(messageType);
     }
 
-    public static Collection<MessageHandler<?>> values() {
+    public static Collection<IMessageHandlerDescriptor> values() {
         return HANDLER_MAP.values();
     }
 
-    @SuppressWarnings("rawtypes")
     @Override
     public void run(String... args) throws Exception {
-        Map<String, MessageHandler> springMap = applicationContext.getBeansOfType(MessageHandler.class);
-        for (MessageHandler messageHandler : springMap.values()) {
+        Map<String, IMessageHandlerDescriptor> springMap = applicationContext.getBeansOfType(IMessageHandlerDescriptor.class);
+        for (IMessageHandlerDescriptor messageHandler : springMap.values()) {
             MessageType messageType = messageHandler.messageType();
             if (HANDLER_MAP.containsKey(messageType)) {
                 // 一种消息平台只接受一个消息处理器（如果接受多个会有处理器执行的顺序问题，会变复杂，暂时不处理这种情况）
